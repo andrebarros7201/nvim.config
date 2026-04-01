96c1a1216d7a67305727cffacd1f0aaeafa54bf6
Andre Barros <andre.barros@banetech.net>
2026-04-01 14:00:51 +0100

    fix: import old options.lua config file

--- lua/options.lua
+++ lua/options.lua
@@ -1,72 +1,54 @@
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-- Short aliases for convenience
+local o = vim.o
+local opt = vim.opt
 
--- Make line numbers default
-vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+-- General behavior
+o.encoding = 'utf-8' -- Use UTF-8 encoding
+o.confirm = true -- Ask to save before quitting unsaved changes
+o.clipboard = 'unnamedplus' -- Sync system clipboard with Neovim
+o.mouse = 'a' -- Enable mouse support
+o.undofile = true -- Save undo history between sessions
+o.scrolloff = 10 -- Keep 10 lines visible above/below cursor
+o.updatetime = 250 -- Faster updates for plugins
+o.timeoutlen = 300 -- Shorter timeout for mapped sequences
 
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
+-- Searching
+o.ignorecase = true -- Ignore case in search...
+o.smartcase = true -- ...unless uppercase is used
+o.hlsearch = true -- Highlight all search matches
+o.incsearch = true -- Show matches as you type
 
--- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
+-- UI
+o.number = true -- Show line numbers
+o.relativenumber = true -- Relative numbers for easy navigation
+o.signcolumn = 'yes' -- Always show sign column
+o.cursorline = false -- Highlight current line
+opt.termguicolors = true -- Enable Terminal Colors
+o.showmode = false -- Don’t show mode (plugin handles that)
+o.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50' -- Mode-specific cursors
+vim.g.have_nerd_font = true -- Enable nerd font
 
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+-- Text and indentation
+opt.tabstop = 4 -- 1 tab = 4 spaces (for display)
+opt.shiftwidth = 4 -- Indent size for << and >>
+opt.softtabstop = 4 -- Spaces per Tab press
+opt.expandtab = true -- Convert tabs to spaces
+opt.smartindent = true -- Smarter auto-indenting for code
+opt.autoindent = true -- Copy indent from current line
+o.smarttab = true -- Use shiftwidth when pressing Tab at line start
+o.breakindent = true -- Indent wrapped lines visually
 
--- Enable break indent
-vim.o.breakindent = true
+-- Text formatting and wrapping
+o.wrap = false -- Don't wrap long lines
+-- o.wrap = true              -- (Optional) Enable for prose or markdown
+-- o.linebreak = true         -- Wrap at word boundaries if wrap = true
 
--- Enable undo/redo changes even after closing and reopening a file
-vim.o.undofile = true
+-- Window and splits
+o.splitright = true -- Vertical splits open to the right
+o.splitbelow = true -- Horizontal splits open below
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
-
--- Decrease update time
-vim.o.updatetime = 250
-
--- Decrease mapped sequence wait time
-vim.o.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.o.splitright = true
-vim.o.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
---
---  Notice listchars is set using `vim.opt` instead of `vim.o`.
---  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---   See `:help lua-options`
---   and `:help lua-guide-options`
-vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.o.confirm = true
-
--- vim: ts=2 sts=2 sw=2 et
+-- Other quality-of-life
+o.list = false -- Show special characters (whitespace, tabs)
+opt.listchars = { tab = '» ', trail = '.', nbsp = '␣' }
+o.inccommand = 'split' -- Live preview of substitutions (:%s)
+o.lazyredraw = true -- Improve performance on macros and large output
