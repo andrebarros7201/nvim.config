fa7a1de505ddffd915d44589f8f542493498975f
Andre Barros <andre.barros@banetech.net>
2026-04-10 13:28:47 +0100

    use primeagen's conform config

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -1,44 +1,32 @@
----@module 'lazy'
----@type LazySpec
 return {
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>cf',
-        function() require('conform').format { async = true, lsp_format = 'fallback' } end,
-        mode = '',
-        desc = 'Format buffer',
+  'stevearc/conform.nvim',
+  opts = {},
+  config = function()
+    require('conform').setup {
+      format_on_save = {
+        timeout_ms = 5000,
+        lsp_format = 'fallback',
       },
-    },
-    ---@module 'conform'
-    ---@type conform.setupOpts
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          return nil
-        else
-          return {
-            timeout_ms = 500,
-            lsp_format = 'fallback',
-          }
-        end
-      end,
       formatters_by_ft = {
-        javascript = { 'prettierd' },
-        javascriptreact = { 'prettierd' },
-        typescript = { 'prettierd' },
-        typescriptreact = { 'prettierd' },
+        c = { 'clang-format' },
+        cpp = { 'clang-format' },
         lua = { 'stylua' },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
       },
-    },
-  },
+      formatters = {
+        ['clang-format'] = {
+          prepend_args = { '-style=file', '-fallback-style=LLVM' },
+          prettier = {
+            command = function(_, ctx)
+              local local_bin = ctx.dirname .. '/node_modules/.bin/prettier'
+              return vim.fn.executable(local_bin) == 1 and local_bin or 'prettier'
+            end,
+          },
+        },
+      },
+    }
+
+    vim.keymap.set('n', '<leader>cf', function() require('conform').format { bufnr = 0 } end)
+  end,
 }
--- vim: ts=2 sts=2 sw=2 et
