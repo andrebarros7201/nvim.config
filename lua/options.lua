7a9c5c69002d312600ca5e000e68bd32e41ee718
Andre Barros <andrebarros7201@gmail.com>
2026-04-11 12:59:02 +0100

    disable relative numbers and add vertical line at 80 char

--- lua/options.lua
+++ lua/options.lua
@@ -2,7 +2,7 @@
 local o = vim.o
 local opt = vim.opt
 
--- General behavior
+-- General behavior -- Add vertical line at 80 char
 o.encoding = 'utf-8' -- Use UTF-8 encoding
 o.confirm = true -- Ask to save before quitting unsaved changes
 o.clipboard = 'unnamedplus' -- Sync system clipboard with Neovim
@@ -12,7 +12,7 @@ o.scrolloff = 10 -- Keep 10 lines visible above/below cursor
 o.updatetime = 250 -- Faster updates for plugins
 o.timeoutlen = 300 -- Shorter timeout for mapped sequences
 o.fileformat = 'unix' -- File format -> lf
-o.ff = 'unix'
+
 -- Searching
 o.ignorecase = true -- Ignore case in search...
 o.smartcase = true -- ...unless uppercase is used
@@ -21,17 +21,19 @@ o.incsearch = true -- Show matches as you type
 
 -- UI
 o.number = true -- Show line numbers
-o.relativenumber = true -- Relative numbers for easy navigation
+o.relativenumber = false -- Relative numbers for easy navigation
 o.signcolumn = 'yes' -- Always show sign column
 o.cursorline = false -- Highlight current line
 opt.termguicolors = true -- Enable Terminal Colors
 o.showmode = false -- Don’t show mode (plugin handles that)
 o.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50' -- Mode-specific cursors
 vim.g.have_nerd_font = true -- Enable nerd font
+opt.cursorline = true -- Highlight current line
+opt.colorcolumn = '80' -- Add vertical line at 80 char -- Add vertical line at 80 char
 
--- Text and indentation
+-- Text and indentation -- Add vertical line at 80 char
 opt.tabstop = 4 -- 1 tab = 4 spaces (for display)
-opt.shiftwidth = 4 -- Indent size for << and >>
+opt.shiftwidth = 4 -- Indent size for << and >> -- Add vertical line at 80 char
 opt.softtabstop = 4 -- Spaces per Tab press
 opt.expandtab = true -- Convert tabs to spaces
 opt.smartindent = true -- Smarter auto-indenting for code
